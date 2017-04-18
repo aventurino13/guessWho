b808eb63d7daa11e923d42d3bea63a6f3605f5ff
Tiffany Love <tnlove_99@yahoo.com>
2017-04-18 13:38:02 -0500

    added css and updated code for guessing person

--- scripts.js
+++ scripts.js
@@ -3,45 +3,47 @@ console.log('sourced');
 var arrayNames = ['Amy', 'Sean', 'Tiffany'];
 var nameShown = "";
 
-function onReady(){
-appendDivs();
-$('#guess').on('click', guessPerson);
-$('.container').on('click', '.person', checkPerson);
+function onReady(){//runs appendDiv function and contains event handlers
+appendDivs();//appends #3 div with images to DOM
+$('#guess').on('click', guessPerson);// event handler for guess button click
+$('.container').on('click', '.person', checkPerson);//event handler for user click photo
+
+
 }
 
 function appendDivs(){
-$('.container').append("<div class = 'person' data-name = 'Amy'>'<img src='https:github.com/aventurino13.png' alt='Profile image of Amy'>'</div>");
-$('.container').append("<div class = 'person' data-name = 'Sean'>'<img src='https:github.com/SF-Felling7.png' alt='Profile image of Sean'>'</div>");
-$('.container').append("<div class = 'person' data-name = 'Tiffany'>'<img src='https:github.com/tiffanylove.png' alt='Profile image of Tiffany'>'</div>");
+  $('.container').append("<div class = 'person' data-name = 'Amy'><img src='https:github.com/aventurino13.png' alt='Profile image of Amy'></div>");
+  $('.container').append("<div class = 'person' data-name = 'Sean'><img src='https:github.com/SF-Felling7.png' alt='Profile image of Sean'></div>");
+  $('.container').append("<div class = 'person' data-name = 'Tiffany'><img src='https:github.com/tiffanylove.png' alt='Profile image of Tiffany'></div>");
 }
 
 function guessPerson() {
-  $('p').empty();
-  var number = randomNumber(1,3);
-  if (number === 1) {
+  $('p').empty();//empty p tag so only new name is shown
+  var number = randomNumber(1,3);//call randomNumber function
+  if (number === 1) {// if randomNumber is 1 will append Amy to DOM
     nameShown = arrayNames[0];
     $('p').append("Who is " + arrayNames[0]);
   }
-  if (number === 2) {
+  if (number === 2) {// if randomNumber is 2 will append Sean to DOM
     nameShown = arrayNames[1];
     $('p').append("Who is " + arrayNames[1]);
   }
-  if (number === 3) {
+  if (number === 3) {// if randomNumber is 3 will append Tiffany to DOM
     nameShown = arrayNames[2];
     $('p').append("Who is " + arrayNames[2]);
   }
-  return nameShown;
 }
 
-function randomNumber(min, max){
+
+function randomNumber(min, max){//calculates random number
     return parseInt(Math.floor(Math.random() * (1 + max - min) + min));
 }
 
-function checkPerson(nameShown) {
-  if ($(this).data('name') === nameShown){
-    alert("Great Job - You Guessed Correct!");
-  } else {alert("Guess again!");
-}
+function checkPerson() {//
+  if (nameShown === $(this).data('name')){
+    alert("Great Job! - You Guessed Correct! You must be a GENIUS");
+  } else {alert("Guess again! -- This time try HARDER!");
+    }
 
 }
 
